160dc343db6124b3e4dc5cddb062826cd2ede9e9
stagas <gstagas@gmail.com>
2026-05-14 18:49:20 +0300

    chore: add sourcemaps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
+  sourcemap: true,
   deps: {
     neverBundle: ['vite'],
   },
